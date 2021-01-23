874de5f89ba469ca0ad5579a71a5b45aca79e805
Emmanuel Uchenna <eunitwap@gmail.com>
2021-01-23 06:39:44 +0100

    Imported fontawesome icons

---
+++ src/components/AppContainer/fontawesome.js
@@ -0,0 +1,10 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// FontAwesome icons
+import { faTwitter, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  faTwitter,
+  faFacebook,
+  faGithub
+);
