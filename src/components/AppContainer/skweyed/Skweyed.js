a598a6fb6249dc30e85f1f912ba8fbb2789e2cc1
Emmanuel Uchenna <eunitwap@gmail.com>
2021-01-23 04:53:47 +0100

    Added basic skeleton

--- src/components/AppContainer/skweyed/Skweyed.js
+++ src/components/AppContainer/skweyed/Skweyed.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './components/assets/Skweyed.css'
+import HeroTextContainer from './components/HeroTextContainer';
+import SocialIconsContainer from './components/SocialIconsContainer';
+import WelcomeInputContainer from './components/WelcomeInputContainer';
 
 class Skweyed extends Component {
 
   render() {
     return (
-      <div>
-
+      <div className="col-12 col-sm-12 col-md-12 col-lg-7 my-auto m-0 p-0">
+        {/* Contents goes here */}
+        <HeroTextContainer />
+        <WelcomeInputContainer />
+        <SocialIconsContainer />
       </div>
     );
   }
