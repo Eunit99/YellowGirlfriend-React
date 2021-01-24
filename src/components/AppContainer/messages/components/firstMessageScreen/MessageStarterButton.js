1988cf868b0dcd25db11e0c3d47f7be93deda599
Eunit <eunithelp@gmail.com>
2021-01-24 07:04:40 +0100

    Created MessageStarterButton component

--- src/components/AppContainer/messages/components/firstMessageScreen/MessageStarterButton.js
+++ src/components/AppContainer/messages/components/firstMessageScreen/MessageStarterButton.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class MessageStarterButton extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+    this.sendMessageButton = this.sendMessageButton.bind(this)
+  }
+
+  sendMessageButton() {
+    console.log(`I as the sendMessageButton is working`)
+  }
 
   render() {
     return (
-      <button className="send-btn soundBtn" id="sendBtn">
+      <button
+        className="send-btn soundBtn"
+        id="sendBtn"
+        onClick={this.sendMessageButton}
+        >
         <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 24 24" width="24" height="24">
           <path fill="none" d="M0 0h24v24H0z" />
           <path
