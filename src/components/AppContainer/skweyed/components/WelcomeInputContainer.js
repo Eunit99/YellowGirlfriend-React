4dca221bb3e53b036fed46008f5f371d7df0db61
Eunit <eunithelp@gmail.com>
2021-01-24 07:03:46 +0100

    Sepearated the WelcomeInputContainer into its respective components

--- src/components/AppContainer/skweyed/components/WelcomeInputContainer.js
+++ src/components/AppContainer/skweyed/components/WelcomeInputContainer.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
+import AppStarterButton from './welcomeInputContainer/AppStarterButton';
+import AppStarterInput from './welcomeInputContainer/AppStarterInput';
 
 class WelcomeInputContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      visitorName: ""
+    }
+    this.appStarterButton = this.appStarterButton.bind(this)
+    this.onChangeHandler = this.onChangeHandler.bind(this)
+  }
+
+  appStarterButton() {
+    console.log(`I as the appStarterButton is working`)
+  }
+
+  onChangeHandler(e) {
+    let typedName = e.target.value;
+
+    this.setState({
+      visitorName: typedName
+    })
+  }
 
   render() {
-    return (
+    return(
       <div className="input-group input-group--intro mt-5">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter first name ..."
-          aria-label="Enter first name ..."
-          aria-describedby="submit-button"
-          id="nameInput"
+        <AppStarterInput
+          onChangeHandler={this.onChangeHandler}
         />
-        <div className="input-group-append">
-          <button className="btn btn-secondary" id="sendBtn" type="button">Start test</button>
-        </div>
+        <AppStarterButton
+          onClick={this.appStarterButton}
+          visitorName={this.state.visitorName}
+        />
       </div>
     );
   }
