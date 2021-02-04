2dc395ef770ea7769ee11879428a49f8ee0715a5
Eunit <eunithelp@gmail.com>
2021-02-04 16:23:48 +0100

    React scroll in Functional component 60% done

--- src/components/AppContainer/messages/Messages.js
+++ src/components/AppContainer/messages/Messages.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import './components/assets/Message.css';
 import ChatsContainer from './components/ChatsContainer';
 import FirstMessageScreen from './components/FirstMessageScreen';
 import '../../AppContainer/assets/css/animate.min.css'
 
 
-class Messages extends Component {
+const Messages = (props) => {
+  const user = props.visitorName,
+    isMessageScreenDisplayed = props.isMessageScreenDisplayed,
+    messageRef = useRef();
 
-  render() {
-    const user = this.props.visitorName,
-      isMessageScreenDisplayed = this.props.isMessageScreenDisplayed;
-
-    return (
-      <div className="col-12 col-sm-12 col-md-12 col-lg-5 p-0 my-auto">
-        { isMessageScreenDisplayed &&
-          <div id="messageContainer" className="message-container animated fadeIn">
-            {/* Content starts here */}
-                <FirstMessageScreen
-                  user={user}
-                />
-                <ChatsContainer />
-          </div>
-        }
-      </div>
-    );
-  }
+  return (
+    <div className="col-12 col-sm-12 col-md-12 col-lg-5 p-0 my-auto">
+      { isMessageScreenDisplayed &&
+        <div
+          messageref={messageRef}
+          ref={messageRef}
+          id="messageContainer"
+          className="message-container animated fadeIn"
+        >
+          {/* Content starts here */}
+          <FirstMessageScreen
+            user={user}
+          />
+          <ChatsContainer />
+        </div>
+      }
+    </div>
+  );
 }
+
 
 export default Messages;
--- src/components/AppContainer/skweyed/components/welcomeInputContainer/AppStarterButton.js
+++ src/components/AppContainer/skweyed/components/welcomeInputContainer/AppStarterButton.js
@@ -1,50 +1,41 @@
-import React, { Component } from 'react';
-import { Link } from 'react-scroll'
+import React from 'react';
+
+const AppStarterButton = (props) => {
+  
+  function handleScrollClick() {
+    console.log(`Click happened!`);
+    props.messageref.current.scrollIntoView({ behaviour: 'smooth'})
+  }
 
 
-class AppStarterButton extends Component {
+  //checking if input is not empty, then allow button interactivity
+  let isButtonDisabled,
+    visitorName = props.visitorName; //Name of visitor typed in the input field in the Skweyed component
 
-  render() {
+  // Check if input field is not empty
+  if (visitorName !== "") {
+    isButtonDisabled = false
+  } else {
+    isButtonDisabled = true
+  }
 
-    //checking if input is not empty, then allow button interactivity
-    let isButtonDisabled,
-    visitorName = this.props.visitorName; //Name of visitor typed in the input field in the Skweyed component
-
-    // Check if input field is not empty
-    if (visitorName !== "") {
-      isButtonDisabled = false
-    } else {
-      isButtonDisabled = true
-    }
-    
-    // For development use only
-    // console.log(`Button is disabled: ${isButtonDisabled}, \n Visitor's name is: ${visitorName}`)
+  // For development use only
+  // console.log(`Button is disabled: ${isButtonDisabled}, \n Visitor's name is: ${visitorName}`)
 
     return (
-        <div
-          className="input-group-append">
-            <button
-              className="btn btn-secondary"
-              id="sendBtn"
-              type="button"
-              onClick={this.props.appStarterButton}
-              disabled={isButtonDisabled}
-            >
-              <Link
-                // activeClass="activeMessage"
-                // delay={500}
-                isDynamic={true}
-                to="messageContainer"
-                spy={false}
-                smooth={true}
-              >
-                Start test
-              </Link>
+      <div
+        className="input-group-append">
+        <button
+          className="btn btn-secondary"
+          id="sendBtn"
+          type="button"
+          onClick={() => {props.appStarterButton(); handleScrollClick() }}
+          disabled={isButtonDisabled}
+        > Start test
             </button>
-        </div>
+      </div>
     );
   }
-}
 
 
 export default AppStarterButton;
