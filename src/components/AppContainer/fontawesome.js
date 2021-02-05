27f1ab350d0004eb9edae93f39c03709a939f9c0
Eunit <eunithelp@gmail.com>
2021-02-05 01:16:50 +0100

    Suggest input name 20% done

--- src/components/AppContainer/fontawesome.js
+++ src/components/AppContainer/fontawesome.js
@@ -2,10 +2,12 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 
 // FontAwesome icons
 import { faTwitter, faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faWarning } from '@fortawesome/free-regular-svg-icons'
 
 library.add(
   faTwitter,
   faFacebook,
   faGithub,
-  faLinkedin
+  faLinkedin,
+  faWarning
 );
--- src/components/AppContainer/usefulinfo/components/suggestinputname/SuggestInputName.js
+++ src/components/AppContainer/usefulinfo/components/suggestinputname/SuggestInputName.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import '../suggestinputname/assets/SuggestInputName.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 class SuggestInputName extends Component {
   constructor(props) {
@@ -27,48 +30,22 @@ class SuggestInputName extends Component {
     // console.log(`isSuggestInputNameDisplayed? ${isSuggestInputNameDisplayed}`)
 
     return (
-      <div className="cookies-consent">
+      <div className="input-name-container">
         {isSuggestInputNameDisplayed &&
           <div className={`
-            cookies-consent-container animated
-            ${isSuggestInputNameDisplayed && "slideInUp"}
+            animated
+            ${isSuggestInputNameDisplayed && "slideInDown"}
           `}>
-            <div className="row cookies-consent-text cookies-consent-text--span">
-              <div className="col-12 mx-1">
-                <div className="cookies-title mb-2 text-left">
-                  Notice
-              </div>
-                <span className="my-2 text-left">
-                  This website or its third-party tools use cookies, which are necessary for its
-                  functioning and required to achieve the purposes illustrated in the
-                <span className="cookies-cta-inline" id="tos"> cookie policy</span>.
-              </span>
-              &nbsp;
-              <span className="my-2 text-left">
-                  You accept the use of cookies by closing or dismissing this notice,
-                  by scrolling this page, by clicking a link or button or by continuing to browse otherwise.
-              </span>
-              </div>
-            </div>
-            <div className="row cookies-consent-text mt-3 mt-sm-4">
-              <div className="col-6">
-                <span
-                  id="tos"
-                  className="cookies-cta cookies-cta--learn"
-                  onClick={this.props.learnMoreAboutCookies}
-                >
-                  Learn more
-              </span>
-              </div>
-              <div className="col-6 text-right">
-                <span
-                  id="cookiesAccept"
-                  className="cookies-cta cookies-cta--accept"
-                  onClick={this.props.cookiesAccept}
-                > Accept
-              </span>
-              </div>
-            </div>
+            {/* Content goes here */}
+          <FontAwesomeIcon icon={['far', 'fawarning']} size="lg" />
+            <span
+              id="closeInfo"
+              className=""
+              onClick={this.props.closeInfo}
+            >
+              Close
+            </span>
+
           </div>
         }
       </div>
@@ -76,8 +53,5 @@ class SuggestInputName extends Component {
   }
 }
 
-SuggestInputName.propTypes = {
-
-};
 
 export default SuggestInputName;
