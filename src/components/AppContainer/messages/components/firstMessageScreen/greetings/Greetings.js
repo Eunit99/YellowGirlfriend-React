ebff5ba165a4cbc5dc9adb5fb2e0bd49763a68ef
Eunit <eunithelp@gmail.com>
2021-02-06 18:45:57 +0100

    Ability to render greeting based on if it is a user or a visitor

--- src/components/AppContainer/messages/components/firstMessageScreen/greetings/Greetings.js
+++ src/components/AppContainer/messages/components/firstMessageScreen/greetings/Greetings.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 
 class Greetings extends Component {
   render() {
-    const user = this.props.user;
-    let greetingText;
-    if (user !== "") {
-      greetingText = `Hello ${user}!`
-    } else {
-      greetingText = `Hello!`
-    }
+    const user = this.props.user,
+      greetWithName = `Hello ${user}!`,
+      greetWithoutName = `Hello!`;
+      var isVisitor;
+
+      if (user !== undefined) {
+        isVisitor = false
+      } else {
+        isVisitor = true
+      }
+    console.log(`isVisitor? ${isVisitor}`)
 
     return (
       <div className="greeting-container">
         <div className="long-into">
-            {greetingText} 
+          {isVisitor ? greetWithoutName : greetWithName}
         </div>
         <div className="small-into">Welcome to yellow girlfriend.</div>
       </div>
